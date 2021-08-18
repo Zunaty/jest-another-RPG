d9d98abc85aca7e1d266234bc2cb21b7571aee92
Victor Perez <zunaty@gmail.com>
2021-08-18 14:36:08 -0600

    battle

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -49,6 +49,19 @@ test('checks if player is alive or not', () => {
     expect(player.isAlive()).toBeFalsy();
 });
 
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
+
 test("gets player's attack value", () => {
     const player = new Player('Dave');
     player.strength = 10;
